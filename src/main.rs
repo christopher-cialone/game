b8495c35dfa664932f7b7345b4ed76acd7b23d2f
Christopher Cialone <ccialone0929@gmail.com>
2024-04-11 08:23:55 -0400

    formatted

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,18 @@ enum Gem {
     Sapphire,
     Ruby,
     Topaz,
-    Onyx, 
+    Onyx,
     Jade,
 }
 fn main() {
-   let gems = [
-    (Gem::Onyx, 25.00),
-    (Gem::Diamond, 100.00),
-    (Gem::Onyx, 50.00),
-    (Gem::Ruby, 10.00)
+    let gems = [
+        (Gem::Onyx, 25.00),
+        (Gem::Diamond, 100.00),
+        (Gem::Onyx, 50.00),
+        (Gem::Ruby, 10.00),
     ];
-    
+
+    for gem in gems {
+        println!("This gem is worth {}", gem.1);
+    }
 }
