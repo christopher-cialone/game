f8f3caa9aae5bd7825a6c43c2fb6f2ee4868ec3b
Christopher Cialone <ccialone0929@gmail.com>
2024-04-11 08:54:55 -0400

    mapping

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use core::fmt;
 
+#[derive(Debug)]
 enum Gem {
     Diamond,
     Sapphire,
@@ -10,7 +11,7 @@ enum Gem {
 }
 
 impl fmt::Display for Gem {
-    fn fmt(&self, f: &mut fmt::Formatter ) -> fmt::Result {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Gem::Diamond => write!(f, "Diamond"),
             Gem::Sapphire => write!(f, "Sapphire"),
@@ -19,19 +20,22 @@ impl fmt::Display for Gem {
             Gem::Onyx => write!(f, "Onyx"),
             Gem::Jade => write!(f, "Jade"),
         }
-
     }
-    
 }
 fn main() {
-    let gems = [
-        (Gem::Onyx, 25.00),
-        (Gem::Diamond, 100.00),
-        (Gem::Onyx, 50.00),
-        (Gem::Ruby, 10.00),
-    ];
+    let mut map = [[false; 5]; 5];
+    println!("{map:?}");
 
-    for gem in gems {
-        println!("This {} is worth {}", gem.0, gem.1);
-    }
+    map[4][2] = true;
+
+    // let gems = [
+    //     (Gem::Onyx, 25.00),
+    //     (Gem::Diamond, 100.00),
+    //     (Gem::Onyx, 50.00),
+    //     (Gem::Ruby, 10.00),
+    // ];
+
+    // for gem in gems {
+    //     println!("This {:?} is worth {}", gem.0, gem.1);
+    // }
 }
