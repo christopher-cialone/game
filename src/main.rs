82dffd3d17a20f70c57ad48693e4e3f096e89b62
Christopher Cialone <ccialone0929@gmail.com>
2024-04-11 08:18:49 -0400

    Arrays

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,11 @@ enum Gem {
     Jade,
 }
 fn main() {
-    let gem1 = (Gem::Onyx, 25.00);
-    let gem2 = (Gem::Jade, 10.00);
-    let gem3 = (Gem::Jade, 15.00):
+   let gems = [
+    (Gem::Onyx, 25.00),
+    (Gem::Diamond, 100.00),
+    (Gem::Onyx, 50.00),
+    (Gem::Ruby, 10.00)
+    ];
     
 }
